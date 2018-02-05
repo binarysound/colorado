fbc3b149420e00033653123164bc968b3a884034
Jeongho Jeong <fiil12@hotmail.com>
2018-02-05 21:58:50 +0900

    Export classes from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './polyfill'
 
+export { default as PitchSystem } from './pitch-system'
+export { default as TuningSystem } from './tuning-system'
+
 export function isValidPitch(pitch: string): boolean {
   return /^[A-G](#|b)?[0-9]?$/.test(pitch)
 }
