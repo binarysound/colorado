81bae14192ed814c7d16e4a83a8d472f1495ee91
HS Lim <bookman1209@gmail.com>
2018-02-09 19:40:22 +0900

    Add factory method `fromSPN` to `Pitch` class

--- src/pitch.spec.ts
+++ src/pitch.spec.ts
@@ -28,3 +28,45 @@ describe('Pitch.isValidSPN', () => {
     expect(Pitch.isValidSPN('F#003')).toBe(false)
   })
 })
+
+describe('Pitch.fromSPN', () => {
+  it('should create correct `Pitch` object for correct pitch notations', () => {
+    expect(Pitch.fromSPN('Cb5').height).toBe(2)
+    expect(Pitch.fromSPN('C5').height).toBe(3)
+    expect(Pitch.fromSPN('C#-3').height).toBe(-92)
+    expect(Pitch.fromSPN('Db-3').height).toBe(-92)
+    expect(Pitch.fromSPN('D5').height).toBe(5)
+    expect(Pitch.fromSPN('D#10').height).toBe(66)
+    expect(Pitch.fromSPN('Eb10').height).toBe(66)
+    expect(Pitch.fromSPN('E5').height).toBe(7)
+    expect(Pitch.fromSPN('E#6').height).toBe(20)
+    expect(Pitch.fromSPN('Fb5').height).toBe(7)
+    expect(Pitch.fromSPN('F6').height).toBe(20)
+    expect(Pitch.fromSPN('F#4').height).toBe(-3)
+    expect(Pitch.fromSPN('Gb4').height).toBe(-3)
+    expect(Pitch.fromSPN('G5').height).toBe(10)
+    expect(Pitch.fromSPN('G#0').height).toBe(-49)
+    expect(Pitch.fromSPN('Ab0').height).toBe(-49)
+    expect(Pitch.fromSPN('A4').height).toBe(0)
+    expect(Pitch.fromSPN('A#1').height).toBe(-35)
+    expect(Pitch.fromSPN('Bb1').height).toBe(-35)
+    expect(Pitch.fromSPN('B4').height).toBe(2)
+    expect(Pitch.fromSPN('B#4').height).toBe(3)
+  })
+
+  it('should throw an error for invalid pitch notations', () => {
+    const errMsg = 'Valid SPN should be given'
+    expect(() => {
+      Pitch.fromSPN('C')
+    }).toThrow(errMsg)
+    expect(() => {
+      Pitch.fromSPN('c')
+    }).toThrow(errMsg)
+    expect(() => {
+      Pitch.fromSPN('C3.5')
+    }).toThrow(errMsg)
+    expect(() => {
+      Pitch.fromSPN('F#003')
+    }).toThrow(errMsg)
+  })
+})
--- src/pitch.ts
+++ src/pitch.ts
@@ -1,8 +1,44 @@
-import { integer } from './util'
+import { ColoradoError, integer } from './util'
 
 export default class Pitch {
   public static isValidSPN(spn: string): boolean {
     return /^[A-G](#|b)?-?([1-9][0-9]+|[0-9])$/.test(spn)
+  }
+
+  public static fromSPN(spn: string): Pitch {
+    if (!Pitch.isValidSPN(spn)) {
+      throw new ColoradoError('Valid SPN should be given')
+    }
+    const spnRegex = /^([A-G](#|b)?)(-?([1-9][0-9]+|[0-9]))$/
+    const match = spnRegex.exec(spn)
+    let height: number
+    switch (match[1]) {
+      // A4 is height 0
+      case 'A': height = 0; break
+      case 'A#':
+      case 'Bb': height = 1; break
+      case 'B': height = 2; break
+      case 'B#': height = 3; break
+      case 'Cb': height = -10; break
+      case 'C': height = -9; break
+      case 'C#':
+      case 'Db': height = -8; break
+      case 'D': height = -7; break
+      case 'D#':
+      case 'Eb': height = -6; break
+      case 'E':
+      case 'Fb': height = -5; break
+      case 'E#':
+      case 'F': height = -4; break
+      case 'F#':
+      case 'Gb': height = -3; break
+      case 'G': height = -2; break
+      case 'G#':
+      case 'Ab': height = -1; break
+    }
+    // Parse octave notation
+    height = height + 12 * (parseInt(match[3], 10) - 4)
+    return new Pitch(height)
   }
 
   @integer
