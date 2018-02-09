1ab65b08db82e42031d1cdfb8da13cba1090e158
HS Lim <bookman1209@gmail.com>
2018-02-09 17:36:27 +0900

    Add static function `Pitch.isValidSPN`

--- src/pitch.spec.ts
+++ src/pitch.spec.ts
@@ -7,3 +7,24 @@ describe('Pitch', () => {
     expect(pitch.height).toBe(3)
   })
 })
+
+describe('Pitch.isValidSPN', () => {
+  it('should return true for correct pitch notations', () => {
+    expect(Pitch.isValidSPN('C#-1')).toBe(true)
+    expect(Pitch.isValidSPN('B3')).toBe(true)
+    expect(Pitch.isValidSPN('Ab4')).toBe(true)
+    expect(Pitch.isValidSPN('F#-13')).toBe(true)
+  })
+
+  it('should return false for invalid pitch notations', () => {
+    expect(Pitch.isValidSPN('C')).toBe(false)
+    expect(Pitch.isValidSPN('D#')).toBe(false)
+    expect(Pitch.isValidSPN('Gb')).toBe(false)
+    expect(Pitch.isValidSPN('H')).toBe(false)
+    expect(Pitch.isValidSPN('c')).toBe(false)
+    expect(Pitch.isValidSPN('X4')).toBe(false)
+    expect(Pitch.isValidSPN('4')).toBe(false)
+    expect(Pitch.isValidSPN('Gk4')).toBe(false)
+    expect(Pitch.isValidSPN('F#003')).toBe(false)
+  })
+})
--- src/pitch.ts
+++ src/pitch.ts
@@ -1,6 +1,10 @@
 import { integer } from './util'
 
 export default class Pitch {
+  public static isValidSPN(spn: string): boolean {
+    return /^[A-G](#|b)?-?([1-9][0-9]+|[0-9])$/.test(spn)
+  }
+
   @integer
   public height: number
 
