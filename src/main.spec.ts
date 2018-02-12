dfdf898ea7bd43163e4e022c23579ad4b77cdd56
HS Lim <bookman1209@gmail.com>
2018-02-12 17:23:42 +0900

    Remove duplicate function `isValidPitch` in `main.ts`

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,16 +1,5 @@
-import { isValidPitch } from './main'
-
-describe('isValidPitch', () => {
-  it('should return true for correct pitch notations', () => {
-    expect(isValidPitch('C')).toBe(true)
-    expect(isValidPitch('D#')).toBe(true)
-    expect(isValidPitch('Gb')).toBe(true)
-    expect(isValidPitch('B3')).toBe(true)
-    expect(isValidPitch('Ab4')).toBe(true)
-  })
-
-  it('should return false for invalid pitch notations', () => {
-    expect(isValidPitch('H')).toBe(false)
-    expect(isValidPitch('c')).toBe(false)
+describe('Dummy', () => {
+  it ('is a dummy test', () => {
+    expect(true).toBe(true)
   })
 })
