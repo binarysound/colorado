2ced240c0d05546d7e42de296bf827da86e3d6dd
Jeongho Jeong <fiil12@hotmail.com>
2018-02-15 23:01:00 +0900

    [#11] Make isInteger polyfill testable

--- src/polyfill.spec.ts
+++ src/polyfill.spec.ts
@@ -1,14 +1,14 @@
-import './polyfill'
+import { isInteger } from './polyfill'
 
 describe('Polyfill', () => {
   describe('Number', () => {
     it('should have `isInteger`', () => {
-      expect(Number.isInteger(3)).toBe(true)
-      expect(Number.isInteger(2.3)).toBe(false)
-      expect(Number.isInteger(NaN)).toBe(false)
-      expect(Number.isInteger('0')).toBe(false)
-      expect(Number.isInteger('123')).toBe(false)
-      expect(Number.isInteger({foo: 'bar'})).toBe(false)
+      expect(isInteger(3)).toBe(true)
+      expect(isInteger(2.3)).toBe(false)
+      expect(isInteger(NaN)).toBe(false)
+      expect(isInteger('0')).toBe(false)
+      expect(isInteger('123')).toBe(false)
+      expect(isInteger({foo: 'bar'})).toBe(false)
     })
   })
 })
--- src/polyfill.ts
+++ src/polyfill.ts
@@ -3,12 +3,16 @@
 /* tslint:disable-next-line:max-line-length */
 /* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger */
 
-interface NumberConstructor {
-  isInteger(value: any): boolean
+declare global {
+  interface NumberConstructor {
+    isInteger(value: any): boolean
+  }
 }
 
-Number.isInteger = Number.isInteger || ((value) => {
+export function isInteger(value) {
   return typeof value === 'number' &&
     isFinite(value) &&
     Math.floor(value) === value
-})
+}
+
+Number.isInteger = Number.isInteger || isInteger
